16593b307020d1cee9b7d5c3cf8d1b9470fb3720
王艳龙 <wangyl@jiedaibao.com>
2018-12-25 14:52:11 +0800

    Change 脚本

--- install_release.sh
+++ install_release.sh
@@ -1,7 +1,7 @@
 ps aux | grep main_ireader | grep -v grep | awk '{print $2}' | xargs kill -9
 export GOPATH=$GOPATH:/data/IReaderServer
 rm ./bin/main_ireader
-git pull origin dev_0.1
+git pull origin dev_1.0
 export GIN_MODE=release
 go install main_ireader
 nohup ./bin /main_ireader  > ret.log &
